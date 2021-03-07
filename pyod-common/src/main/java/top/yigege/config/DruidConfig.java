99f3beea53db24a2c2f12ce8a7b432148b9c10c1
pengyige <1309902313@qq.com>
2021-03-07 14:34:05 +0800

    添加druid监控

--- pyod-common/src/main/java/top/yigege/config/DruidConfig.java
+++ pyod-common/src/main/java/top/yigege/config/DruidConfig.java
@@ -1,7 +1,11 @@
 package top.yigege.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +24,42 @@ public class DruidConfig {
     @Bean
     public DataSource druidDataSource() {
         return new DruidDataSource();
+    }
+
+
+
+    /**
+     * 注册一个StatViewServlet
+     *
+     * @return servlet registration bean
+     */
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+                new StatViewServlet(), "/druid/*");
+
+        servletRegistrationBean.addInitParameter("loginUsername", "root");
+        servletRegistrationBean.addInitParameter("loginPassword", "Pyod@2020!");
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     *
+     * @return filter registration bean
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(
+                new WebStatFilter());
+
+        // 添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        // 添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
     }
 }
